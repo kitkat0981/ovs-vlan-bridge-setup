53d2463ab56c5af09581aa819b761615e4bdec58
kitkat0981 <kitkat0981@gmail.com>
2025-05-27 09:11:48 -0400

    Update setup-ovs.sh
    made changes to remove the assignment of IP from the netplan config to the ovs setup.

--- setup-ovs.sh
+++ setup-ovs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Remove existing bridge if it exists
+# Delete existing bridge if exists
 ovs-vsctl --if-exists del-br br0
 
-# Create OVS bridge
+# Create the bridge
 ovs-vsctl add-br br0
 
 # Add trunk ports
@@ -19,3 +19,22 @@ ip link set br0 up
 ip link set ens4 up
 ip link set ens5 up
 ip link set ens6 up
+
+# Wait briefly for bridge creation
+sleep 1
+
+# Create VLAN interface on top of br0
+ip link add link br0 name br0.2201 type vlan id 2201
+
+# Assign IP address
+ip addr add 10.10.3.2/24 dev br0.2201
+
+# Bring up the VLAN interface
+ip link set br0.2201 up
+
+# Add default route
+ip route add default via 10.10.3.1
+
+# Optional DNS
+echo "nameserver 10.10.3.22" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
